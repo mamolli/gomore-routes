8dd727f312cb252b1d3dac5650802de519707141
me@me.com <me@me.com>
2021-05-29 14:39:41 +0200

    C-command, adding data

--- routes/src/core.clj
+++ routes/src/core.clj
@@ -1,12 +1,31 @@
+(defn deep-merge [& maps]
+  ; for merging maps recursively 
+  ; (deep-merge {:a {:b 10}} {:a {:d 5}}) => {:a {:b 10 :d 5}}
+  (letfn [(m [& xs]
+            (if (some #(and (map? %) (not (record? %))) xs)
+              (apply merge-with m xs)
+              (last xs)))]
+    (reduce m maps)))
 
 ; patterns for matching
-(def parse-input-re #"([Q])\s*(\w+)?\s*(\w+)?\s*([0-9-]+)?\s*(\d+)?")
-(def parse-search-re #"([S])")
+; todo: partial matching, for better help/error control flow
+(def re-command #"([C])\s+(\w+)\s+(\w+)\s+([0-9-]+)\s+(\d+)")
+(def re-search #"([S])")
+
 
 (defn parse-line [line]
-  (let [[_ command & args] (re-find parse-input-re line)]
+  (let [[_ command & args] (re-find re-command line)]
     (prn "Parsing line: " line " => " command args)
     [command args]))
+
+(defn command-C [args]
+  "Process C command args and return partial map"
+  "Example return: {:Gdansk {:Copenhagen {:2010-11-03 10}}}"
+  (when (not-any? nil? args); none of the args can be nil
+    (let [[city-src city-dst date num-seats] args]
+      {(keyword city-src)
+       {(keyword city-dst)
+        {(keyword date) (Integer/parseInt num-seats)}}}))) ;using bigint as it parses string
 
 (defn parse-input []
   (loop [conn-data {}
@@ -15,5 +34,17 @@
           [command args] (parse-line line)]
       (case command
         "Q" (prn "Exiting, last state:" conn-data log)
-        "C" (assoc conn-data :last line)))))
+        "C" (recur (deep-merge conn-data (command-C args)) (conj log line))))))
+
+
+(def lines
+  {:a "C Gdn Cop 2010 20"
+   :missing "C Gdn Cop 2010"
+   :b "C Gdn Cop 2011 15"})
+
+(-> (:a lines)
+    parse-line
+    second ;second is args vector
+    command-C)
+
 
